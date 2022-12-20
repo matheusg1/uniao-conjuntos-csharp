a8e0c99e7ad70ca08e73e2b64e08858880476bf8
matheus.silva <tecmatheusg@hotmail.com>
2022-12-20 12:42:43 -0300

    primeiro commit

---
+++ Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniaoConjuntosCsharp
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Aluno aluno &&
+                   Id == aluno.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniaoConjuntosCsharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<Aluno> cursoA = new HashSet<Aluno>();
+            HashSet<Aluno> cursoB = new HashSet<Aluno>();
+            HashSet<Aluno> cursoC = new HashSet<Aluno>();
+
+            cursoA.Add(new Aluno { Id = 21 });
+            cursoA.Add(new Aluno { Id = 35 });
+            cursoA.Add(new Aluno { Id = 22 });
+
+            cursoB.Add(new Aluno { Id = 21 });
+            cursoB.Add(new Aluno { Id = 50 });
+
+            cursoC.Add(new Aluno { Id = 42 });
+            cursoC.Add(new Aluno { Id = 35 });
+            cursoC.Add(new Aluno { Id = 13 });
+
+            var alunosUnicos = new HashSet<Aluno>(cursoA);
+            alunosUnicos.UnionWith(cursoB);
+            alunosUnicos.UnionWith(cursoC);
+            Console.WriteLine(alunosUnicos.Count);
+        }
+    }
+}
